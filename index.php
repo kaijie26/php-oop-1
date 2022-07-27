f3253d7d2b104bcf22afe90ab1fea568e2ffd515
kaijie26 <chenkaijie26@gmail.com>
2022-07-27 17:50:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+
+<?php
+    class Movie {
+        public $title;
+
+        public $genre;
+
+        public $likes;
+
+        public $accessibility;
+
+        public $vision;
+
+        public function __construct($_title, $_genre, $_vision){
+            $this->title = $_title;
+            $this->genre = $_genre;
+            $this->vision = $_vision;
+
+        }
+
+        public function setAccessibility() {
+            if($this->vision === 'Unrestricted') {
+                $this->accessibility = 'Visione aperta a tutti';
+
+            }else{
+                $this->accessibility = 'Visione per un pubblico adulto';
+
+            } 
+        }
+
+        public function getAccessibility() {
+            return $this->accessibility;
+        }
+
+
+    }
+
+    $terminator = new Movie('Terminator', 'Action', 'Unrestricted');
+    $terminator->likes = '8888';
+    var_dump($terminator->getAccessibility())
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1> <?php echo $terminator->title?> </h1>
+    <div> <?php echo $terminator->getAccessibility() ?> </div>
+    
+</body>
+</html>
